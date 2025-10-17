46dcfbfa32cac30fc007cf7ac594371986722b9b
Lívia Almada <almada.livia@gmail.com>
2025-10-17 08:56:41 -0300

    Implement basic structure of BPlusTree class

---
+++ java/BPlusTree.java
@@ -0,0 +1,7 @@
+public class BPlusTree<K extends Comparable<K>, V> {
+    public BPlusTree(int order) { }
+    public void insert(K key, V value) { }
+    public V search(K key) { return null; }
+    public boolean remove(K key) { return false; }
+    public void display() { }
+}
