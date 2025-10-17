0724828ff986cbf0d329c89ad64abd6db2a569e8
Lívia Almada <almada.livia@gmail.com>
2025-10-17 08:57:38 -0300

    Create ExtensibleHash class with basic methods

---
+++ java/ExtensibleHash.java
@@ -0,0 +1,7 @@
+public class ExtensibleHash<K, V> {
+    public ExtensibleHash(int bucketSize) { }
+    public void insert(K key, V value) { }
+    public V search(K key) { return null; }
+    public boolean remove(K key) { return false; }
+    public void display() { }
+}
